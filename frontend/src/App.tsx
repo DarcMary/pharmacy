bb017d264da152b214080bf638bd5a6827e6c014
Darc Mary <darcmary12@gmail.com>
2025-09-18 05:22:41 -0300

    refactor: reorganiza fluxo de navegação
    - Move login para página inicial
    - Atualiza rotas protegidas
    - Melhora experiência de usuário inicial
    - Adiciona redirecionamentos baseados em autenticação

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,49 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
 import { CartProvider } from './contexts/CartContext';
 import Header from './components/Header';
 import HomePage from './pages/HomePage';
 import ProductDetailPage from './pages/ProductDetailPage';
 import CartPage from './pages/CartPage';
+import { LoginPage } from './pages/LoginPage';
+import { RegisterCustomerPage } from './pages/RegisterCustomerPage';
+import PrivateRoute from './components/PrivateRoute';
+import { WelcomePage } from './pages/WelcomePage';
 
 function App() {
   return (
-    <CartProvider>
-      <Router>
-        <div className="min-h-screen bg-gray-50">
-          <Header />
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/product/:id" element={<ProductDetailPage />} />
-            <Route path="/cart" element={<CartPage />} />
-          </Routes>
-        </div>
-      </Router>
-    </CartProvider>
+    <AuthProvider>
+      <CartProvider>
+        <Router>
+          <div className="min-h-screen bg-gray-50">
+            <Header />
+            <Routes>
+              <Route path="/" element={<WelcomePage />} />
+              <Route path="/products" element={
+                <PrivateRoute>
+                  <HomePage />
+                </PrivateRoute>
+              } />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/register" element={<RegisterCustomerPage />} />
+              <Route path="/product/:id" element={
+                <PrivateRoute>
+                  <ProductDetailPage />
+                </PrivateRoute>
+              } />
+              <Route 
+                path="/cart" 
+                element={
+                  <PrivateRoute allowedRoles={['customer']}>
+                    <CartPage />
+                  </PrivateRoute>
+                } 
+              />
+            </Routes>
+          </div>
+        </Router>
+      </CartProvider>
+    </AuthProvider>
   );
 }
 
